ab2e9bd24d093a4e988ff542f89263c41204c937
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-08-14 13:53:58 +0530

    contact us info update

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -124,7 +124,7 @@ export default function ContactUs() {
       <div className="container map justify-content-center d-flex py-4">
         <div className="row">
           <div className="col-12 d-lg-block d-none">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15087.101852961821!2d73.0166431!3d19.0296134!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3eaaa53c4e3%3A0x9459161291e7ded5!2sTerna%20Engineering%20College!5e0!3m2!1sen!2sin!4v1714456799929!5m2!1sen!2sin" 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.778299320158!2d72.99732867885837!3d19.073482736055464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c1482b5b2087%3A0x4cb38efd3c7362f5!2sArenja%20Corner%2C%20Palm%20Beach%20Rd%2C%20Sector%2017%2C%20Vashi%2C%20Navi%20Mumbai%2C%20Maharashtra%20400703!5e0!3m2!1sen!2sin!4v1723623631174!5m2!1sen!2sin" 
               width="980"
               height="400"
               style={{ border: '2px solid silver' }}
@@ -135,7 +135,7 @@ export default function ContactUs() {
             </iframe>
           </div>
           <div className="col-12 d-md-block d-none d-lg-none">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4756650888185!2d73.02050307497653!3d19.042812982154942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3db5e2c85cd%3A0xef26c52d7d73816e!2sSIES%20Graduate%20School%20of%20Technology!5e0!3m2!1sen!2sin!4v1711868027053!5m2!1sen!2sin"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.778299320158!2d72.99732867885837!3d19.073482736055464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c1482b5b2087%3A0x4cb38efd3c7362f5!2sArenja%20Corner%2C%20Palm%20Beach%20Rd%2C%20Sector%2017%2C%20Vashi%2C%20Navi%20Mumbai%2C%20Maharashtra%20400703!5e0!3m2!1sen!2sin!4v1723623631174!5m2!1sen!2sin"
               width="600"
               height="400"
               style={{ border: '2px solid silver' }}
@@ -146,7 +146,7 @@ export default function ContactUs() {
             </iframe>
           </div>
           <div className="col-12 d-md-none d-sm-block">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4756650888185!2d73.02050307497653!3d19.042812982154942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3db5e2c85cd%3A0xef26c52d7d73816e!2sSIES%20Graduate%20School%20of%20Technology!5e0!3m2!1sen!2sin!4v1711868027053!5m2!1sen!2sin"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.778299320158!2d72.99732867885837!3d19.073482736055464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c1482b5b2087%3A0x4cb38efd3c7362f5!2sArenja%20Corner%2C%20Palm%20Beach%20Rd%2C%20Sector%2017%2C%20Vashi%2C%20Navi%20Mumbai%2C%20Maharashtra%20400703!5e0!3m2!1sen!2sin!4v1723623631174!5m2!1sen!2sin"
               width="100%"
               height="400"
               style={{ border: '2px solid silver' }}
@@ -164,7 +164,7 @@ export default function ContactUs() {
           <div className="col-12 col-lg-5 contact-left">
             <h2 className="py-2" style={{ color: '#00365E' }}>Inquiries</h2>
             <p className="py-2" style={{ fontWeight: 'light', fontSize: '1.2rem' }}>
-              For any inquiries, questions or commendations, please call: 123-456-7890 or fill out the following form
+              For any inquiries, questions or commendations, please call: 9833758761 or fill out the following form
             </p>
             <form onSubmit={submitHandler}>
               <div>
@@ -202,17 +202,17 @@ export default function ContactUs() {
             <div className="address">
               <h2 className="py-2" style={{ color: '#00365E' }}>Head Office</h2>
               <p className="py-2" style={{ fontWeight: 'light', fontSize: '1.2rem' }}>
-                322 Arenja Corner, plot no. 73 <br />
+                322, ARENJA CORNER, PLOT NO. 73 <br />
                 SEC-17 VASHI, NAVI-MUMBAI
               </p>
               <p className="py-2" style={{ fontWeight: 'light', fontSize: '1.2rem' }}>
-                info@mysite.com <br />
-                Tel: 123-456-7890 <br />
-                Fax: 123-456-7890
+                buildwellengineering.com <br />
+                Contact No: 9833758761 <br />
+                Mail: Bwell.engineering@gmail.com
               </p>
             </div>
             <div className="quote mt-auto">
-              <h2 className="py-2" style={{ color: '#00365E' }}>Get a quote: 123-456-7890</h2>
+              <h2 className="py-2" style={{ color: '#00365E' }}>Get a quote: 9833758761</h2>
             </div>
           </div>
         </div>
